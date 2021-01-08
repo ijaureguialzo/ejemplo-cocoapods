b23862c6a999a245e5b2d1a238511119ffe6fa13
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-01-08 19:26:21 +0100

    Proyecto nuevo

---
+++ EjemploCocoaPods/EjemploCocoaPods/EjemploCocoaPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjemploCocoaPodsApp.swift
+//  EjemploCocoaPods
+//
+//  Created by Ion Jaureguialzo Sarasola on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct EjemploCocoaPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
